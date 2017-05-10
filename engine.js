6e60ebd33cdc434fc36a186b0fb9694a12c5d578
Lucas Neves <lcneves@gmail.com>
2017-05-10 21:10:37 +0000

    WIP

---
+++ engine.js
@@ -0,0 +1,15 @@
+/*
+ * engine.js
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
+ *
+ * Engine for the Livre project based on HTML5.
+ */
+
+'use strict';
+
+
+module.exports = function (options) {
+  const theme = options.theme;
+
+  function makeShell() {
+    
---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+ * index.js
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
+ *
+ * Entry point for the Livre-HTML engine.
+ * Initializes the Livre client with the engine contained in this directory.
+ *
+ */
+
+'use strict';
+
+module.exports = function (options) {
+
+  // These options were passed by the theme.
+  const engine = require('./engine.js')(options);
+
+  require('livre-client')({
+    engine: engine
+  });
+};
+
